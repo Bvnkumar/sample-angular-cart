415807612cf47fe3335db2e42e9b4f83cf4d9de7
srinivasareddybakka11 <srinivasareddybakka@gmail.com>
2020-10-11 00:05:50 +0530

    signup code.

--- sample-cart/src/app/signup/signup.component.ts
+++ sample-cart/src/app/signup/signup.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder,FormGroup,Validators} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
+import {Router} from '@angular/router';
+
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +10,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
-
-  constructor() { }
+  signUpForm : FormGroup;
+  constructor(private formBuilder : FormBuilder,
+    private http : HttpClient,
+    private route : Router) {
+    
+   }
 
   ngOnInit() {
+    this.signUpForm=this.formBuilder.group({
+    firstName :["",[Validators.required]],
+    username :["",[Validators.required]],
+    password :["",[Validators.required,Validators.maxLength(4)]]
+    })
   }
+
+  signupClick = () =>{
+    console.log("this signup form",this.signUpForm);
+    const data =this.signUpForm.value;
+    console.log(" data ",data);
+    this.http
+    .post("http://localhost:8081/users/create", data)
+
+    .subscribe((data) => {
+      if (data) {
+        this.route.navigate(["/welcome"]);
+      } 
+    });
+
+  } ;
 
 }
