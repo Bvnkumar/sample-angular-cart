5fac923d72f4a068294a72b09e2c93e09f9a79d7
Bvnkumar <bvnkumar007@gmail.com>
2020-10-20 00:14:41 -0400

    Added cart count and getting data based on cart items

--- sample-cart/src/app/cart/cart.component.ts
+++ sample-cart/src/app/cart/cart.component.ts
@@ -1,23 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import CartService from "../cart.service";
 
 @Component({
-  selector: 'app-cart',
-  templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.scss']
+  selector: "app-cart",
+  templateUrl: "./cart.component.html",
+  styleUrls: ["./cart.component.scss"],
 })
 export class CartComponent implements OnInit {
-
-  constructor( private http : HttpClient) { }
-  cartList ;
+  constructor(private http: HttpClient, private cartService: CartService) {}
+  cartList;
 
   ngOnInit() {
     this.http
-    .post("http://localhost:8081/product/getAllOrdersWithIds",[1,2])
-    .subscribe((data)=>{
-      console.log("cartList",data);
-      this.cartList=data;
-    });
+      .post(
+        "http://localhost:8081/product/getAllOrdersWithIds",
+        this.cartService.getItems()
+      )
+      .subscribe((data) => {
+        console.log("cartList", data);
+        this.cartList = data;
+      });
   }
-
 }
