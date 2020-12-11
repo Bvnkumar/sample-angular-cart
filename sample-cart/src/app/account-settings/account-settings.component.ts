31edfed1dc41084c6809c3c5efa1d2dcf0449e36
Bvnkumar <bvnkumar007@gmail.com>
2020-12-11 16:21:33 -0500

    removed console log

--- sample-cart/src/app/account-settings/account-settings.component.ts
+++ sample-cart/src/app/account-settings/account-settings.component.ts
@@ -1,33 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import {UserService} from '../user.service';
-import lodash from 'lodash';
+import { Component, OnInit } from "@angular/core";
+import { UserService } from "../user.service";
+import lodash from "lodash";
 @Component({
-  selector: 'app-account-settings',
-  templateUrl: './account-settings.component.html',
-  styleUrls: ['./account-settings.component.scss']
+  selector: "app-account-settings",
+  templateUrl: "./account-settings.component.html",
+  styleUrls: ["./account-settings.component.scss"],
 })
 export class AccountSettingsComponent implements OnInit {
   userData;
   ordersList;
-  constructor(
-    private userService : UserService
-  ) { }
+  constructor(private userService: UserService) {}
 
   ngOnInit() {
-   this.userService.accountSettings()
-    .subscribe( (data) =>{
-      console.log("user Details",data);
-      this.userData=data;
-      this.ordersList=this.userData.orderList.map(item=>{
+    this.userService.accountSettings().subscribe((data) => {
+      console.log("user Details", data);
+      this.userData = data;
+      this.ordersList = this.userData.orderList.map((item) => {
         return item.product;
-      })
-      console.log("OrderList1",this.ordersList);
-      this.ordersList=lodash.flattenDeep(this.ordersList)
-      console.log("OrderList",this.ordersList);
-      console.log(" UserData",this.userData);
-      console.log(" this.ordersList ",  this.ordersList);
+      });
+      console.log("OrderList1", this.ordersList);
+      this.ordersList = lodash.flattenDeep(this.ordersList);
+      console.log(" UserData", this.userData);
+      console.log(" this.ordersList ", this.ordersList);
     });
   }
-  
-
 }
